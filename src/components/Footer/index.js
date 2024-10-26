7e41147beed534e155ae752b6696257c68f529b1
Artik Bharoliya <artikbharoliya7@gmail.com>
2024-10-26 14:39:07 -0700

    implement the footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,33 @@
 import styles from './Footer.module.scss';
 import globalStyles from '../../styles/globals.module.scss';
 import Image from 'next/image';
+import { Col, Container, Row, Stack } from 'react-bootstrap';
+import Link from 'next/link';
 
 export default function Footer() {
   return (
     <>
-      <div>
-        Footer
-      </div>
+      <footer className={styles.footerContainer}>
+        <Container>
+          <Row>
+            <Col md={8} xs={12}>
+              <Image
+                src="/assets/logo.png"
+                alt="Logo"
+                width={200}
+                height={100}
+              />
+            </Col>
+            <Col md={4} xs={12}>
+              <Stack gap={3}>
+                <Link href="/">Home</Link>
+                <Link href="/contact">Contact Us</Link>
+                <Link href="/about">About Us</Link>
+              </Stack>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
     </>
   );
 }
