f964f47d933f6380a573d3b44b3768489db7578d
Artik Bharoliya <artikbharoliya7@gmail.com>
2024-10-26 14:40:34 -0700

    implements carousel component

---
+++ src/components/Carousel/index.js
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import Image from 'next/image';
+
+function ControlledCarousel() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <Image
+          src={'/assets/carousel/1.jpeg'}
+          alt="First slide"
+          width={800}
+          height={600}
+        />
+        {/* <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption> */}
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image
+          src={'/assets/carousel/2.jpeg'}
+          alt="First slide"
+          width={800}
+          height={600}
+        />
+        {/* <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption> */}
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image
+          src={'/assets/carousel/4.jpeg'}
+          alt="First slide"
+          width={800}
+          height={600}
+        />
+        {/* <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption> */}
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default ControlledCarousel;
