9cfffce3781ac89f03e604014324bdf3b4225719
Artik Bharoliya <artikbharoliya7@gmail.com>
2024-11-03 10:55:33 -0800

    fix some styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ export default function Home() {
             <ControlledCarousel />
           </Col>
         </Row>
-        <MarketingCopy />
         <Products />
+        <MarketingCopy />
       </Container>
     </>
   );
