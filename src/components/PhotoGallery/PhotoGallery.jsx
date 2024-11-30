1068023f949012e58573d84b87b6faa2c37286c8
Artik Bharoliya <artikbharoliya7@gmail.com>
2024-11-30 13:28:59 -0800

    add photo gallery to projects tab

---
+++ src/components/PhotoGallery/PhotoGallery.jsx
@@ -0,0 +1,31 @@
+
+import Image from 'next/image';
+import styles from './PhotoGallery.module.scss';
+import StackGrid from 'react-stack-grid';
+
+export function PhotoGallery({ photos }) {
+  return (
+    <div className="py-5">
+      <StackGrid columnWidth={600}>
+        {photos?.map((photo, index) => {
+          return (
+            <>
+              <div className={styles.photoContainer} horizontal>
+                <Image 
+                  src={photo}
+                  style={{ 
+                    objectFit: 'cover',
+                  }}
+                  fill
+                  sizes='100vw'
+                  height={0}
+                  width={0}
+                />
+              </div>
+            </>
+          );
+        })}
+      </StackGrid>
+    </div>
+  );
+}
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,11 +1,31 @@
-import Products from "@/components/Products";
+import { PhotoGallery } from "@/components/PhotoGallery/PhotoGallery";
 import TitleSection from "@/components/TitleSection";
 
 export default function ProjectPage() {
+  const photos = [
+    "/assets/projects/1.jpg",
+    "/assets/projects/2.jpg",
+    "/assets/projects/3.jpg",
+    "/assets/projects/4.jpg",
+    "/assets/projects/5.jpg",
+    "/assets/projects/6.jpg",
+    "/assets/projects/7.jpg",
+    "/assets/projects/8.jpg",
+    "/assets/projects/9.jpg",
+    "/assets/projects/10.jpg",
+    "/assets/projects/11.jpg",
+    "/assets/projects/12.jpg",
+    "/assets/projects/13.jpg",
+    "/assets/projects/14.jpg",
+    "/assets/projects/15.jpg",
+    "/assets/projects/16.jpg",
+    "/assets/projects/17.jpg",
+    "/assets/projects/18.jpg",
+  ];
   return (
     <>
       <TitleSection title={"Projects"}>
-        Hello world
+        <PhotoGallery photos={photos} />
       </TitleSection>
     </>
   );
